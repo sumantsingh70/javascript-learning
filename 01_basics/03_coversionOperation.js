7d61235dcb8aa83220ff67623b7c3729a95b1ed9
Sumant Singh <singhsumant581@gmail.com>
2024-07-19 10:58:23 +0000

    conversion of datatypes

---
+++ 01_basics/03_coversionOperation.js
@@ -0,0 +1,31 @@
+let score = "Sumant"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+
+
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber);
+
+// 33 => Number
+// "33abc"=>Nan
+// true => 1 false =>0
+
+let isLoggedIn = "Sumant"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(booleanIsLoggedIn);
+
+// 1 => true ; 0 => false
+// "" => false
+// "Sumant" => true
+
+let someNumber = 33
+
+let stringNumber  = String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
