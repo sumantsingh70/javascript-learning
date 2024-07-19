647210a6f117544c4d74231b3161a4031392784f
Sumant Singh <singhsumant581@gmail.com>
2024-07-19 11:29:14 +0000

    comparisons

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,25 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+//null treated as 0
+
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// comparison and equality check are two different things
+//avoid this type of comparisons
+
+// ===  strict check
+console.log("2" === 2);
