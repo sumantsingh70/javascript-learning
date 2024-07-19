32926bce7e423b04d79c6f9f8e44751840230ab5
Sumant Singh <singhsumant581@gmail.com>
2024-07-19 03:25:34 +0000

    datatypes

---
+++ 01_basics/02-datatypes.js
@@ -0,0 +1,23 @@
+"use strict";     //treat all JS code as newer version
+
+//. alert("hello"). // we are using nodejs, not browser
+
+let name = "Sumant"
+let age = 21
+let isLoggedIn = false
+let state;
+
+//number => 2 to power 53
+//bigint
+
+//string => ""
+//boolean
+//null => standalone value
+//undefined => value not assigned
+//symbol => to find uniqueness
+
+
+// object
+
+console.log(typeof null)        //object
+console.log(typeof undefined) //undefined
